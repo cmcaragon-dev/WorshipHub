e47dd8f7cb6d63d004aadc61dc718110cb80b5ec
cmcaragon-dev <cmcaragon@gmail.com>
2026-08-11 15:51:19 +0300

    Update song.js

--- js/song.js
+++ js/song.js
@@ -16,7 +16,8 @@ import {
 import {
     doc,
     getDoc,
-    updateDoc
+    updateDoc,
+    serverTimestamp
 } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
 // ==========================================
