becee822eba0540726faeae87c4ad7b270bc6b24
cmcaragon-dev <cmcaragon@gmail.com>
2026-08-16 15:28:04 +0300

    Update songs.js

--- js/songs.js
+++ js/songs.js
@@ -268,7 +268,7 @@ title: "Mahal na Mahal Kita Panginoon",
         category: "Praise",
         language: "Tagalog",
         file: "songs/mahalnamahalkita.html"
-    }
+    },
 	{id:28,        
 title: "Tayo Na Sama Sama",
         artist: "Meha Harvest",
