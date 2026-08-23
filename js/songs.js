5b3f8862b712f1a0809e849a4829240d9fbefcdb
cmcaragon-dev <cmcaragon@gmail.com>
2026-08-23 14:36:21 +0300

    Update songs.js

--- js/songs.js
+++ js/songs.js
@@ -380,16 +380,8 @@ title: "Happy Day",
         category: "Praise",
         language: "English",
         file: "songs/happyday.html"
-	},
-	{id:46,        
-title: "You, You Are God",
-        artist: "Gateway Worship",
-        key: "C",
-        category: "Praise",
-        language: "English",
-        file: "songs/youyouaregod.html"
 	}
-];
+	];
 songs.sort((a, b) =>
     a.title.localeCompare(b.title)
 );
